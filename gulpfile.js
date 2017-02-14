473ff0d77a1784ec723b20187095430f780846d2
Krzysztof Wielicki <kwielicki@e-point.pl>
2017-02-14 17:10:17 +0100

    BAŁTEX-20 Dodanie komponentu CTA, usunięcie blockquote oraz animated canvas.

--- gulpfile.js
+++ gulpfile.js
@@ -107,7 +107,6 @@ gulp.task('js-plugins', function() {
 		devJSPath + 'plugins/bootstrap/bs.util.js',
 		devJSPath + 'plugins/bootstrap/bs.modal.js',
 		devJSPath + 'plugins/custom-scroll-bar/jquery.mCustomScrollbar.min.js',
-		devJSPath + 'plugins/animated-canvas/animated-canvas.js',
 		devJSPath + 'plugins/jquery-match-height/jquery-match-height.js'
 	])
 	.pipe(plumber())
